603ab778796a4f5601973c58da91bc17282fb032
Xavier Soler Parés <xaviersolerpares@gmail.com>
2014-11-06 16:57:28 +0100

    Commit inicial
    Xcode project created

---
+++ AppDeProva/AppDeProvaTests/AppDeProvaTests.swift
@@ -0,0 +1,36 @@
+//
+//  AppDeProvaTests.swift
+//  AppDeProvaTests
+//
+//  Created by Xavier Soler Parés on 6/11/14.
+//  Copyright (c) 2014 Xavier Soler Parés. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AppDeProvaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
